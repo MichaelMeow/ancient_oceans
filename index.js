298b208ddef419e6b20e50e611522ebc7d6430cf
Brendan Shea and Michael Harmon <brendan@>
2018-09-26 16:43:39 -0700

    Create grid with animations.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import './sass/styles.scss';
-import ancient-oceans from '../assets/video/ancient-oceans.mp4';
-
-
-$('.txt').html(function(i, html) {
-  var chars = $.trim(html).split("");
-
-  return '<span>' + chars.join('</span><span>') + '</span>';
-});
+// import './sass/styles.scss';
+// import ancient-oceans from '../assets/video/ancient-oceans.mp4';
+//
+//
+// $('.txt').html(function(i, html) {
+//   var chars = $.trim(html).split("");
+//
+//   return '<span>' + chars.join('</span><span>') + '</span>';
+// });
